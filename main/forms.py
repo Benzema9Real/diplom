1258f282383bb6a4670c1071e3c850c412bfbb65
Nikita7773000 <footballfceasports@gmail.com>
2024-07-15 17:50:15 +0600

    lklkk

--- main/forms.py
+++ main/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django_recaptcha.fields import ReCaptchaField
+
 from .models import Article, Grade, Comment, Support
 
 
